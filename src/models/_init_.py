2506c3a84a1d4533133cf42b6bc4d7fbd0777183
Muhammed Yavuzhan CANLI <mycc21@bath.ac.uk>
2025-08-05 16:40:07 +0300

    Removed large model files from Git tracking (now ignored)

--- src/models/_init_.py
+++
@@ -1,9 +0,0 @@
-# src/models/__init__.py
-
-"""Machine learning models for casino customer segmentation"""
-
-from .segmentation import CustomerSegmentation
-from .promotion_rf import PromotionResponseModel
-from .model_registry import ModelRegistry
-
-__all__ = ['CustomerSegmentation', 'PromotionResponseModel', 'ModelRegistry']
